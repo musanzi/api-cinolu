2614ae083d153b810d53b7f1ca7588336ebdbef0
Wilfried Musanzi <musanziw.m@gmail.com>
2026-06-02 11:33:13 +0200

    chore: refactor auth module to remove session auth dependency and add session serializer

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,15 +5,15 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { UsersModule } from '../identity/users/users.module';
 import { AuthEmailService } from './services/auth-email.service';
-import { AuthPasswordService } from './services/auth-password.service';
-import { SessionAuthModule } from '@musanzi/nestjs-session-auth';
-import { SYSTEM_RBAC_POLICY } from '@/modules/auth/system-rbac';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { SessionSerializer } from './session.serializer';
 
 @Global()
 @Module({
-  imports: [UsersModule, SessionAuthModule.forRoot({ policies: [SYSTEM_RBAC_POLICY] })],
+  imports: [UsersModule, PassportModule, JwtModule],
   controllers: [AuthController],
-  providers: [AuthService, AuthPasswordService, LocalStrategy, GoogleStrategy, AuthEmailService],
+  providers: [AuthService, LocalStrategy, GoogleStrategy, SessionSerializer, AuthEmailService],
   exports: [AuthService]
 })
 export class AuthModule {}
