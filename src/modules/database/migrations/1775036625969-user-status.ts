528e2cfd04e3232411da4659553fa5bab3245fb0
Wilfried Musanzi <musanziw.m@gmail.com>
2026-06-02 11:36:31 +0200

    refactor: standardize formatting and improve readability in migration files

--- src/modules/database/migrations/1775036625969-user-status.ts
+++ src/modules/database/migrations/1775036625969-user-status.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class UserStatus1775036625969 implements MigrationInterface {
-    name = 'UserStatus1775036625969'
+  name = 'UserStatus1775036625969';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` ADD \`status\` enum ('entrepreneur', 'investor', 'partner', 'other') NOT NULL DEFAULT 'other'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`user\` ADD \`status\` enum ('entrepreneur', 'investor', 'partner', 'other') NOT NULL DEFAULT 'other'`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`status\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`status\``);
+  }
 }
