a1db030ef7af07af0aeeba32c5295776ee66532c
Wilfried Musanzi <musanziw.m@gmail.com>
2026-07-16 16:33:05 +0200

    feat: integrate MySQL session store for improved session management

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import session from 'express-session';
 import passport from 'passport';
 import { Logger } from 'nestjs-pino';
+import MySQLStore from 'express-mysql-session';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -18,12 +19,23 @@ async function bootstrap(): Promise<void> {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
   });
+  const MysqlSessionStore = MySQLStore(session);
+  const sessionStore = new MysqlSessionStore({
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
   app.use(
     session({
+      store: sessionStore,
       saveUninitialized: false,
-      resave: false,
-      secret: process.env.SESSION_SECRET as string,
-      cookie: { maxAge: +process.env.SESSION_MAX_AGE, httpOnly: true }
+      secret: process.env.SESSION_SECRET,
+      cookie: {
+        maxAge: +process.env.SESSION_MAX_AGE,
+        httpOnly: true
+      }
     })
   );
   app.use(passport.initialize());
